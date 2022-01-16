b997839d9edc5240bb1879c60fd869597e979471
pawaret <62021393@up.ac.th>
2022-01-17 04:25:45 +0700

    Add account page (Inventory)

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,9 +1,88 @@
-import React from "react";
+import { React, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-function Account() {
+import { ReactComponent as Copy } from "../assets/copy.svg";
+import { ReactComponent as Check } from "../assets/check.svg";
+import Owned from "./Owned";
+
+function Account({ account }) {
+  const [path, setPath] = useState();
+  const [copy, setCopy] = useState();
+  const [buttonDisabled, setButtonDisabled] = useState();
+
+  function watchPath(path) {
+    setPath(path);
+  }
+
+  function copyAddress() {
+    navigator.clipboard.writeText(account);
+    setCopy(true);
+    setButtonDisabled(true);
+    setTimeout(() => {
+      setCopy(false);
+      setButtonDisabled(false);
+    }, 2000);
+  }
+
   return (
-    <div className="h-full w-full bg-white">
-      <div></div>
+    <div className="h-full w-full text-wh flex justify-center my-10 bg-background">
+      <div className="h-full w-10/12">
+        <div className="flex flex-col space-y-5 items-center">
+          <div className="flex space-x-3 text-text">
+            <p>{`${account.slice(0, 5)} ... ${account.slice(-6)}`}</p>
+            <button onClick={copyAddress} disabled={buttonDisabled}>
+              {copy === true ? <Check /> : <Copy />}
+            </button>
+          </div>
+          <div className="w-full flex flex-col items-center">
+            <div className="h-18 flex items-center text-text">
+              <Link
+                to=""
+                onClick={() => {
+                  watchPath("");
+                }}
+                className="relative h-full flex px-10 items-center"
+              >
+                <p>On Sale</p>
+                {path === "" ? (
+                  <span className="absolute h-1 w-full bottom-0 left-0 rounded-t-lg bg-primary"></span>
+                ) : null}
+              </Link>
+              <Link
+                to={"owned"}
+                onClick={() => {
+                  watchPath("owned");
+                }}
+                className="relative h-full flex px-10 items-center"
+              >
+                <p>Owned</p>
+                {path === "owned" ? (
+                  <span className="absolute h-1 w-full bottom-0 left-0 rounded-t-lg bg-primary"></span>
+                ) : null}
+              </Link>
+              <Link
+                to={"created"}
+                onClick={() => {
+                  watchPath("created");
+                }}
+                className="relative h-full flex px-10 items-center"
+              >
+                <p>Created</p>
+                {path === "created" ? (
+                  <span className="absolute h-1 w-full bottom-0 left-0 rounded-t-lg bg-primary"></span>
+                ) : null}
+              </Link>
+            </div>
+            <span className="h-1 w-full divider-x"></span>
+          </div>
+        </div>
+        <div className="h-full flex justify-center items-center text-3xl text-white">
+          <Routes>
+            <Route path="" element={<p>No items to display</p>} />
+            <Route path=":owned" element={<Owned />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
