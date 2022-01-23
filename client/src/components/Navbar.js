489807d37de1ef88f5d9078da389f2ebdf941047
pawaret <62021393@up.ac.th>
2022-01-24 05:12:37 +0700

    Fix navbar position

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,7 +12,7 @@ function Navbar({ connectWallet, account }) {
   }
 
   return (
-    <div className="fixed w-full h-18 grid grid-cols-8 items-center text-white shadow-lg bg-background">
+    <div className="sticky top-0 w-full h-18 grid grid-cols-8 items-center text-white shadow-lg bg-background">
       <div className="col-span-2 px-5">
         <Link
           to="/"
