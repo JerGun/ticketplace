1971f2c6fdceb4cacf7aba5185e831e162df0ce6
pawaret <62021393@up.ac.th>
2022-01-28 03:48:50 +0700

    deploy market

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Market = artifacts.require("./Market.sol")
+const SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Market = artifacts.require("./Market.sol");
+const NFT = artifacts.require("./NFT.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(Market);
+  deployer.deploy(NFT);
 };
