ace30d3122027a94a55abe519ecc92ee3b3e4695
pawaret <62021393@up.ac.th>
2022-01-30 01:08:43 +0700

    Hotfix

--- client/src/components/QueryNavLink.js
+++ client/src/components/QueryNavLink.js
@@ -3,7 +3,6 @@ import { useLocation, NavLink } from "react-router-dom";
 
 function QueryNavLink({ to, ...props }) {
   let location = useLocation();
-  console.log(location);
   return <NavLink to={to + location.search} {...props} />;
 }
 
