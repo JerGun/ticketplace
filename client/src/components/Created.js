e17980c01595c6e837d29f1979cec632fee7a9da
pawaret <62021393@up.ac.th>
2022-02-16 22:53:48 +0700

    Adjust item card

--- client/src/components/Created.js
+++ client/src/components/Created.js
@@ -2,12 +2,13 @@ import { React, useState, useEffect } from "react";
 import Web3 from "web3";
 import axios from "axios";
 import Event from "../contracts/Event.json";
-import QueryNavLink from "./QueryNavLink";
+import { Link } from "react-router-dom";
 
 import { ReactComponent as More } from "../assets/icons/more.svg";
+import { ReactComponent as Location } from "../assets/icons/location.svg";
 
 function Created() {
-  const [tickets, setTickets] = useState([]);
+  const [events, setEvents] = useState([]);
   const [loadingState, setLoadingState] = useState();
 
   useEffect(() => {
@@ -37,49 +38,57 @@ function Created() {
         console.log(data2);
         let item = {
           tokenId: i.tokenId,
-          supply: i.supply,
           image: meta.data.image,
           name: meta.data.name,
           link: meta.data.link,
+          description: meta.data.description,
+          location: meta.data.location,
           startDate: meta.data.startDate,
           endDate: meta.data.endDate,
-          description: meta.data.description,
+          startTime: meta.data.startTime,
+          endTime: meta.data.endTime,
         };
         return item;
       })
     );
     console.log(items);
 
-    setTickets(items);
+    setEvents(items);
     setLoadingState("loaded");
   };
 
-  if (loadingState === "loaded" && !tickets.length)
+  if (loadingState === "loaded" && !events.length)
     return <h1 className="py-10 px-20 text-3xl">No assets owned</h1>;
   return (
     <div className="flex justify-center">
       <div className="p-4">
         <div className="h-auto w-full grid grid-cols-2 gap-5 pb-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {tickets.map((ticket, i) => (
+          {events.map((event, i) => (
             <div
               key={i}
               className="h-fit w-full rounded-lg shadow-lg float-right bg-modal-button"
             >
-              <QueryNavLink to={`/ticket/${ticket.tokenId}`}>
+              <Link to={`/event/${event.tokenId}`}>
                 <div className="p-3 space-y-3 shadow-lg">
                   <div className="h-72 w-full">
                     <img
-                      src={ticket.image}
+                      src={event.image}
                       alt=""
                       className="h-full w-full object-cover rounded-lg"
                     />
                   </div>
-                  <div className="w-full flex flex-col items-start">
-                    <p className="text-text">{ticket.supply} total</p>
-                    <p className="w-9/12 truncate">{ticket.name}</p>
+                  <div className="w-full flex flex-col items-start space-y-1">
+                    <p className="text-sm text-primary">
+                      {event.startDate} - {event.endDate}
+                    </p>
+                    <p className="w-9/12 truncate">{event.name}</p>
+                    <div className="w-9/12 flex items-center truncate space-x-1">
+                      <Location className="h-4 w-4" />
+                      <p>{event.location}</p>
+                    </div>
                   </div>
                 </div>
-              </QueryNavLink>
+              </Link>
               <div className="px-3 py-2 flex items-center text-text">
                 <button className="scale-75 hover:text-white">
                   <More />
