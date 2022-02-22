5a08148097edbbf93de398aa958f8535d24c5279
pawaret <62021393@up.ac.th>
2022-02-23 01:39:53 +0700

    Adjust server production url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://ticketplace-server.herokuapp.com/"
+    ? "https://ticketplace-server.herokuapp.com"
     : "http://localhost:9000";
