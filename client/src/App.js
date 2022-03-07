7513aad38279daf70d4f61afb67b5ae354de7aaa
pawaret <62021393@up.ac.th>
2022-03-07 19:44:17 +0700

    Hotfix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { ReactComponent as Close } from "./assets/icons/close.svg";
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,3 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
-const Market = artifacts.require("./Market.sol");
-const TicketERC721 = artifacts.require("./TicketERC721.sol");
-const Ticket = artifacts.require("./Ticket.sol");
 const Event = artifacts.require("./Event.sol");
 
 module.exports = function (deployer) {
