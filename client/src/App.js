b237b01ea8724ca5537f5f8572aa20fae3012c81
pawaret <62021393@up.ac.th>
2022-03-07 08:26:21 +0700

    Adjust router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import { ReactComponent as Close } from "./assets/icons/close.svg";
 
